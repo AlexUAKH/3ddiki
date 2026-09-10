57ccd012e60a3034af4893392eb6c96304c5e96c
Oleksandr <alex.l@greatdev.net>
2026-09-10 12:43:39 +0300

    radobot

--- admin/src/middleware/guestMiddleware.ts
+++ admin/src/middleware/guestMiddleware.ts
@@ -1,7 +1,11 @@
 import { ERouteNames } from "@/router/router.types";
 
 export default function guest({ store }: { store: any }) {
-  console.log("guest middleware: ");
+  console.log(
+    "guest middleware: ",
+    "loggedin: ",
+    store.loggedIn ? "true" : "false",
+  );
 
   if (store.loggedIn) {
     return {
--- admin/src/stores/auth.ts
+++ admin/src/stores/auth.ts
@@ -103,6 +103,7 @@ export const useAuthStore = defineStore(
     persist: {
       // afterRestore: (ctx) => {
       afterHydrate: (ctx) => {
+        console.log("3ddiki_access_token: ", ctx.store.accessToken);
         if (ctx.store.accessToken)
           Cookies.set("3ddiki_access_token", ctx.store.accessToken, {
             expires: 365,
